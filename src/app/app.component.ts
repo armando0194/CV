9c40c4605b6aec081737bc54e8d0021c6a0d2fe9
Armando <armando@Armandos-MacBook-Pro.local>
2019-08-28 20:12:57 -0600

    Added dependencies

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { ResumeService } from './services/resume/resume.service';
+import { Resume } from './models/resume';
+import { delay } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +10,32 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'CV';
+  config: any;
+  fullpageApi: any;
+  
+  private loading = true;
+  private debounceTime = 100;
+
+  private resume: Resume;
+
+  constructor(private resumeService: ResumeService) {
+    this.config = {
+      anchors: ['home', 'aboutme'],
+      menu: '#menu'
+    };
+
+    this.loading = true;
+    this.resumeService.getResume().pipe(delay(this.debounceTime)).subscribe(
+      data => { 
+        this.resume = data;
+        this.loading = false;
+        console.log(this.resume);
+      },
+      err => console.log(err)
+    );
+  }
+
+  getRef(fullPageRef) {
+    this.fullpageApi = fullPageRef;
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,35 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoadingComponent } from './components/loading/loading.component';
+
+import { AngularFullpageModule } from '@fullpage/angular-fullpage';
+import { AboutmeComponent } from './components/aboutme/aboutme.component';
+import { HeroComponent } from './components/hero/hero.component';
+
+import { HttpClientModule } from '@angular/common/http';
+import { HTTP_INTERCEPTORS } from '@angular/common/http';
+import { ResumeService } from './services/resume/resume.service';
+import { ChartsModule } from 'ng2-charts';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoadingComponent,
+    AboutmeComponent,
+    HeroComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AngularFullpageModule,
+    HttpClientModule,
+    ChartsModule
   ],
-  providers: [],
+  providers: [
+    ResumeService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
